13007faeed1a802acf9d91ae4c454a2a9660c26e
Mike K <michaelkucukarslan@gmail.com>
2024-03-27 07:11:03 -0700

    Added tournament classes to begin project

--- models/tournament.py
+++ models/tournament.py
@@ -1,10 +1,6 @@
-# Create tournament
-# Create name
-# A venue
-# A start and end date
-# A list of registered players
-# A number of rounds
-# A number corresponding to the current round being played
+import json
+
+from .player import Player
 
 class Tournament:
     """A local tournament"""
@@ -17,3 +13,27 @@ class Tournament:
         self.round_number = round_number
         self.current_round = 0
         self.filepath = filepath
+
+        # Load File
+        if filepath and not name:
+            with open(filepath) as fp:
+                data = json.load(fp)
+                self.name = data["name"]
+                self.players = [
+                    Player(**player_dict) for player_dict in data["players"]
+                ]
+        elif not filepath:
+            self.save()
+    # Save file
+    def save(self):
+        """Serialize the players and save them to the tournament into a JSON file"""
+
+        with open(self.filepath, "w") as fp:
+            json.dump(
+                {"name": self.name, "players": [p.serialize() for p in self.players]},
+                fp,
+            )
+            
+# Don't need to finish the whole class to go to the next class
+# Think about how to add one feature at a time
+# see how the project handles the clubs and the players for making the tournament
--- screens/main_menu.py
+++ screens/main_menu.py
@@ -16,6 +16,7 @@ class MainMenu(BaseScreen):
     def get_command(self):
         while True:
             print("Type C to create a club or a club number to view/edit it.")
+            print("Type T to view current tournaments or create a new tournament.")
             print("Type X to exit.")
             value = self.input_string()
             if value.isdigit():
@@ -24,5 +25,7 @@ class MainMenu(BaseScreen):
                     return NoopCmd("club-view", club=self.clubs[value - 1])
             elif value.upper() == "C":
                 return NoopCmd("club-create")
+            elif value.upper() == "T":
+                return NoopCmd("tournament-screen")
             elif value.upper() == "X":
                 return ExitCmd()
