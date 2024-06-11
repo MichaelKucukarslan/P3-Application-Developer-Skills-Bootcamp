b4be3ad50b7b5bc552de9606235bf5412ad4518e
Mike K <michaelkucukarslan@gmail.com>
2024-06-11 13:46:43 -0700

    Got rounds to display without Pandas

--- screens/tournaments/view.py
+++ screens/tournaments/view.py
@@ -1,4 +1,5 @@
 from ..base_screen import BaseScreen
+import pandas as pd
 
 class TournamentView(BaseScreen):
     """Screen displayed when viewing the tournament menu"""
@@ -14,10 +15,26 @@ class TournamentView(BaseScreen):
         print("End Date: " + self.tournament.end_date)
         print("Player ID Numbers:")
         for player in self.tournament.players:
-            # print(player)
             print(f"\tID: {player.chess_id} \tName: {player.name}")
-        print("Current Round: " + str(self.tournament.current_round))
-        
+        if (self.tournament.current_round == 0):
+            print("Tournament Completed")
+        else:
+            print("Current Round: " + str(self.tournament.current_round))
+        for round_index, round_data in enumerate(self.tournament.rounds, start=1):
+            print(f"Round {round_index}:")
+            for match_index, match in enumerate(round_data, start=1):
+                players = " vs ".join(match['players'])
+                completed = "Completed" if match["completed"] else "Not Completed"
+                winner = match.get("winner")
+                winner_display = f"Winner: {winner}" if winner else "Tie Game"
+                print(f"  Match {match_index}: {players}")
+                print(f"    Status: {completed}")
+                print(f"    {winner_display}")
+            print()        
+    data = []
+    
     def get_command(self):
         """Child classes must implement this method. It must return a Command."""
+        # If the tournament is done just display it
+        # If the tournament is NOT done allow to continue it
     pass
