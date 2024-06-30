e4012be126f768b2158b2dcfe905d96f21952dd9
Mike K <michaelkucukarslan@gmail.com>
2024-06-29 20:25:08 -0700

    Removed unused method

--- models/printer.py
+++ models/printer.py
@@ -3,13 +3,6 @@
 class Printer:
     def __init__(self):
         pass
-    
-    def print_ranking(self, data):
-        length_max = 12
-        print("| Player      | Points      |")
-        print("| ----------- | ----------- |")
-        for player in data:
-            self.print_row_of_info([player.player.name, str(player.points)], length_max)
 
     def print_row_of_info(self, data, length_max):
         pad_char = ' '
